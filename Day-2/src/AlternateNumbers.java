9662000c43cc389c5054beb9d6084bdb9b77bc78
aslam2003 <93483643+aslam2003@users.noreply.github.com>
2023-07-11 13:48:39 +0530

    Add files via upload

---
+++ Day-2/src/AlternateNumbers.java
@@ -0,0 +1,12 @@
+
+public class AlternateNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i;
+       for(i=1;i<=99;i+=2) {
+    	   System.out.print(i+" ");
+       }
+	}
+
+}
---
+++ Day-2/src/Numbers1to100.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Numbers1to100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           Scanner sc=new Scanner(System.in);
+           int i;
+           for(i=1;i<=100;i++) {
+        	   System.out.print(i+" ");
+           }
+	}
+
+}
